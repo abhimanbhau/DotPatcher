940210cf4b87a1f64ae22446f5a44b4a2ed80b28
Abhimanbhau Kolte <abhimanbhau@gmail.com>
2015-03-27 22:24:50 +0530

    repo init

---
+++ DotPatcher/PatchTools.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Mono.Cecil;
+using System.IO;
+using System.Linq;
+using Mono.Cecil.Cil;
+using System.Reflection;
+using System.Windows.Forms;
+using MalwareInjection;
+
+namespace DotPatcher
+{
+	public static class PatchTools
+	{
+
+		public static void patchGeneric (bool patchMethod, string path, string message)
+		{
+			AssemblyDefinition assembly = AssemblyDefinition.ReadAssembly (path);
+			TypeDefinition mainClass = assembly.MainModule.Types [1];
+			MethodDefinition mainMethod = mainClass.Methods.OfType<MethodDefinition> ()
+				.Where (m => m.Name == "Main").Single ();
+
+			MethodInfo injectMethod;
+			MethodReference method;
+			if (patchMethod) { // console
+				injectMethod =
+					typeof(Console).GetMethod ("WriteLine", new []{ typeof(string) });
+				method = assembly.MainModule.Import (injectMethod);
+			} else {
+				injectMethod =
+					typeof(MessageBox).GetMethod ("Show", new []{ typeof(string) });
+				method = assembly.MainModule.Import (injectMethod);
+			}
+			ILProcessor proc = mainMethod.Body.GetILProcessor ();
+			var instr = proc.Create (OpCodes.Ldstr, message);
+			var instr2 = proc.Create (OpCodes.Call, method);
+			var start = mainMethod.Body.Instructions [0];
+
+
+			mainMethod.Body.GetILProcessor ().InsertBefore (start, instr);
+			mainMethod.Body.GetILProcessor ().InsertAfter (instr, instr2);
+
+			if (!patchMethod) {
+				var pop = proc.Create (OpCodes.Pop);
+				mainMethod.Body.GetILProcessor ().InsertAfter (instr2, pop);
+			}
+
+			if (patchMethod) {
+				if (File.Exists (path.Replace (".exe", "").Trim () + "-con" + ".exe"))
+					File.Delete (path.Replace (".exe", "").Trim () + "-con" + ".exe");
+				assembly.Write (path.Replace (".exe", "").Trim () + "-con" + ".exe");
+			} else {
+				if (File.Exists (path.Replace (".exe", "").Trim () + "-pop" + ".exe"))
+					File.Delete (path.Replace (".exe", "").Trim () + "-pop" + ".exe");
+				assembly.Write (path.Replace (".exe", "").Trim () + "-pop" + ".exe");
+			}
+		}
+
+		public static void wipeAssembly (string path)
+		{
+			AssemblyDefinition assembly = AssemblyDefinition.ReadAssembly (path);
+			TypeDefinition mainClass = assembly.MainModule.Types [1];
+			MethodDefinition mainMethod = mainClass.Methods.OfType<MethodDefinition> ()
+					.Where (m => m.Name == "Main").Single ();
+
+			var ret = mainMethod.Body.GetILProcessor ().Create (OpCodes.Ret);
+			mainMethod.Body.GetILProcessor ().InsertBefore (mainMethod.Body.Instructions [0], ret);
+			if (File.Exists (path.Replace (".exe", "").Trim () + "-empty" + ".exe"))
+				File.Delete (path.Replace (".exe", "").Trim () + "-empty" + ".exe");
+			assembly.Write (path.Replace (".exe", "").Trim () + "-empty" + ".exe");
+		}
+
+		public static void patchMalware (string path)
+		{
+			AssemblyDefinition assembly = AssemblyDefinition.ReadAssembly (path);
+			TypeDefinition mainClass = assembly.MainModule.Types [1];
+			MethodDefinition mainMethod = mainClass.Methods.OfType<MethodDefinition> ()
+				.Where (m => m.Name == "Main").Single ();
+
+			var injectMethod =
+				typeof(MalwareRunner).GetMethod ("DoBadThings", new []{ typeof(string) });
+			var method = assembly.MainModule.Import (injectMethod);
+
+			var start = mainMethod.Body.GetILProcessor().Create (OpCodes.Ldstr, "");
+			var instr = mainMethod.Body.GetILProcessor ().Create (OpCodes.Call, method);
+			mainMethod.Body.GetILProcessor ().InsertBefore (mainMethod.Body.Instructions [0], start);
+			mainMethod.Body.GetILProcessor ().InsertAfter (start, instr);
+
+			assembly.Write (path.Replace (".exe", "").Trim () + "-malware" + ".exe");
+		}
+	}
+}
+
---
+++ DotPatcher/Program.cs
@@ -0,0 +1,66 @@
+﻿/* Copyleft 2015 Abhimanbhau Kolte
+ * The file is part of DotPatcher project and is licensed under MIT License
+ * 
+ * @TODO
+ * # Injection Demos -> Do pseudo malacious tasks
+ * Inject protection -> part of DotProtect project
+ * 
+ * */
+
+
+using System;
+using System.IO;
+
+namespace DotPatcher
+{
+	public class Program
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("DotPatcher v1.0.1b");
+			Console.WriteLine ("Enter exe path to be patched : ");
+			string exePath = Console.ReadLine ();
+			if (!File.Exists (exePath))
+				throw new FileNotFoundException ();
+			if (args.Length > 0) {
+				if (args [0] == "oneshot") {
+					Console.WriteLine ("Patching the exe, 'oneshot'-way");
+					PatchTools.patchGeneric (true, exePath, "DotPatcher - OneShot patch");
+					PatchTools.patchGeneric (false, exePath, "DotPatcher - OneShot patch");
+					PatchTools.wipeAssembly (exePath);
+				}
+			}else {
+				try {
+					Console.WriteLine ("Patching Methods\n1. Inject Custom Message" +
+					"\n2. Inject Popup message\n3. Wipe out whole assembly\n4. Malware Patch");
+					short patchMethod = Int16.Parse (Console.ReadLine ());
+
+					Console.WriteLine ("Enter custom message : ");
+					string message = Console.ReadLine ();
+
+					switch (patchMethod) {
+					case 1:
+						PatchTools.patchGeneric (true, exePath, message);
+						break;
+
+					case 2:
+						PatchTools.patchGeneric (false, exePath, message);
+						break;
+
+					case 3:
+						PatchTools.wipeAssembly (exePath);
+						break;
+
+					case 4:
+						PatchTools.patchMalware(exePath);
+						break;
+					}
+					Console.WriteLine ("\nAbhimanbhau Kolte (c)CopyLeft 2015");
+				} catch (Exception ex) {
+					File.WriteAllText ("crash-" + DateTime.Now.ToLongDateString () + ".log", 
+						ex.Message + "\n" + ex.StackTrace);
+				}
+			}
+		}
+	}
+}
